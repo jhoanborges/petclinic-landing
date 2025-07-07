145a67ed2b5761e436dc7357c4c2af7b5e62fc58
Jhoan Borges <jhoan.borges@hexagun.mx>
2025-07-07 17:49:35 -0600

    built

--- components/navbar/logo.tsx
+++ components/navbar/logo.tsx
@@ -4,25 +4,15 @@ import Image from 'next/image';
 import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
 
-  /**
-   * Logo component that adapts to light/dark mode
-   *
-   * This component will render a placeholder/default logo during SSR, and
-   * will render the correct logo for the current theme once the component
-   * has mounted. This is due to the fact that the `useTheme` hook only returns
-   * a valid theme on the client, and not on the server.
-   */
 export const Logo = () => {
-  const { theme, resolvedTheme } = useTheme();
+  const { resolvedTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
-  // useEffect only runs on the client, so now we can safely show the UI
   useEffect(() => {
     setMounted(true);
   }, []);
 
   if (!mounted) {
-    // Return a placeholder or default logo during SSR
     return (
       <Image 
         src="/loho-horizontal.svg" 
